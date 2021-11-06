76cb28f582c230d853d55d95c2625c33f43a7d26
datnd <datdinh201799@gmail.com>
2021-11-06 16:08:40 +0700

    init spring

---
+++ library_management_demo/src/main/java/com/example/library_management_demo/LibraryManagementDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.library_management_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryManagementDemoApplication.class, args);
+    }
+
+}
---
+++ library_management_demo/src/test/java/com/example/library_management_demo/LibraryManagementDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.library_management_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
